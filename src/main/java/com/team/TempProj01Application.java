096c0310de46dc9426ee09cec75b02ff9da2e6c9
jiyeong-kim <jiyeong-kim-@naver.com>
2023-03-21 17:47:18 +0900

    초 기 셋 팅 완 료 - 2023.03.21

---
+++ src/main/java/com/team/TempProj01Application.java
@@ -0,0 +1,13 @@
+package com.team;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TempProj01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TempProj01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/team/TempProj01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TempProj01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
